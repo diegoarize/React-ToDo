7a817ec11d5a1a3f36e22b68efc69f12368c3b41
diegoarize <diegoarize@outlook.com>
2017-01-12 14:29:57 -0300

    Complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -12,5 +12,13 @@ describe('TodoApp', () => {
     expect(TodoApp).toExist();
   });
 
+  it('Sould add todo to the todos state on handleAddTodo', () => {
+    var todoText = 'test test';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+    expect(todoApp.state.todos[0].text).toBe(todoText); 
+  });
 
 });
