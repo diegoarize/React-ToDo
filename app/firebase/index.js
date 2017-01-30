f2b4abe8d8f963606c528d55894540b55ec2623c
diegoarize <diegoarize@outlook.com>
2017-01-29 21:36:49 -0300

    move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyD_0W5UDYBXvE1t6N7ASqYlmGvPnMtcrEo",
-      authDomain: "arize-todo-app.firebaseapp.com",
-      databaseURL: "https://arize-todo-app.firebaseio.com",
-      storageBucket: "arize-todo-app.appspot.com",
-      messagingSenderId: "945776645210"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
 } catch (e) {
